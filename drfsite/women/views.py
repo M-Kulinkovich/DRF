345ec4552294b381e0f7d18d7bd410c1ccadf07e
M-Kulinkovich <wait_z@mail.ru>
2023-02-28 11:18:18 +0300

    Create simple django_rest_framework app

---
+++ drfsite/women/views.py
@@ -0,0 +1,28 @@
+from django.forms import model_to_dict
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Women, Category
+from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
+from .serializers import WomenSerializer
+
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly,)
